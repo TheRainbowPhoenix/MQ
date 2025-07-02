6122059a6173ce4765ef3234443e2e91c9f3d384
Lephenixnoir <sebastien.michelland@protonmail.com>
2025-07-02 20:15:38 +0200

    machine: add XYRAM - Boson X frames now rendering

--- core/machine.c
+++ core/machine.c
@@ -131,6 +131,9 @@ void mq_machine_initialize(mqMachine *mach, int initializeKind)
         void *ostk = mq_memory_allocBuffer(mach->memory, "OSTK", 512 << 10);
         mq_memory_createBlock(mach->memory, 0x8c0f0000, 512 << 10, ostk);
         mq_memory_createBlock(mach->memory, 0xac0f0000, 512 << 10, ostk);
+        /* XYRAM */
+        void *xyram = mq_memory_allocBuffer(mach->memory, "XYRAM", 16 << 10);
+        mq_memory_createBlock(mach->memory, 0xe500e000, 16 << 10, xyram);
 
         // TODO[machine]: Reasonable heap address on fx-CG?!
         mach->system.heapAddress = layout_heap;
