7faaa56208f7e02cf83ff3ea9bdc54f843e7f7d8
Yann MAGNIN <yann.magnin@protonmail.com>
2025-09-02 10:39:54 +0200

    rtc: support RCR2 START/RESET/ADJ

--- modules/rtc.c
+++ modules/rtc.c
@@ -65,7 +65,7 @@ static int int16(uint16_t bcd)
 
 // =================== Interrupts =======//
 
-static void rtc_interrupt_refresh(mqMachine *mach, mqRTC *RTC)
+static void rtc_refresh_interrupts(mqMachine *mach, mqRTC *RTC)
 {
     int alarm_match;
     int alarm_count;
@@ -114,232 +114,6 @@ static void rtc_interrupt_refresh(mqMachine *mach, mqRTC *RTC)
         mq_intc_setInterruptStatus(mach, MQ_INT_RTC_ATI, true);
 }
 
-//==================== REGS ===========//
-
-static void write_RSECCNT(struct mqMMIO *io, u32 addr, u32 value, int size)
-{
-    mqMachine *mach = io->userdata;
-    mqRTC *RTC = mach->modules[moduleID];
-    (void)addr;
-    (void)size;
-
-    // on the real hardware, no verification is performed on the value. You
-    // can write 0x7f and the register will not perform anything until a
-    // new second is elapsed
-    RTC->RSECCNT = value & 0x7f;
-}
-
-static void write_RMINCNT(struct mqMMIO *io, u32 addr, u32 value, int size)
-{
-    mqMachine *mach = io->userdata;
-    mqRTC *RTC = mach->modules[moduleID];
-    (void)addr;
-    (void)size;
-
-    // same as RSECCNT, no verification until the next refresh
-    RTC->RMINCNT = value & 0x7f;
-}
-
-static void write_RHRCNT(struct mqMMIO *io, u32 addr, u32 value, int size)
-{
-    mqMachine *mach = io->userdata;
-    mqRTC *RTC = mach->modules[moduleID];
-    (void)addr;
-    (void)size;
-
-    // same as RSECCNT, no verification until the next refresh
-    RTC->RHRCNT = value & 0x3f;
-}
-
-static void write_RWKCNT(struct mqMMIO *io, u32 addr, u32 value, int size)
-{
-    mqMachine *mach = io->userdata;
-    mqRTC *RTC = mach->modules[moduleID];
-    (void)addr;
-    (void)size;
-
-    // same as RSECCNT, no verification until the next refresh
-    RTC->RWKCNT = value & 0x07;
-}
-
-static void write_RDAYCNT(struct mqMMIO *io, u32 addr, u32 value, int size)
-{
-    mqMachine *mach = io->userdata;
-    mqRTC *RTC = mach->modules[moduleID];
-    (void)addr;
-    (void)size;
-
-    // same as RSECCNT, no verification until the next refresh
-    RTC->RDAYCNT = value & 0x3f;
-}
-
-static void write_RMONCNT(struct mqMMIO *io, u32 addr, u32 value, int size)
-{
-    mqMachine *mach = io->userdata;
-    mqRTC *RTC = mach->modules[moduleID];
-    (void)addr;
-    (void)size;
-
-    // same as RSECCNT, no verification until the next refresh
-    RTC->RMONCNT = value & 0x1f;
-}
-
-static void write_RYRCNT(struct mqMMIO *io, u32 addr, u32 value, int size)
-{
-    mqMachine *mach = io->userdata;
-    mqRTC *RTC = mach->modules[moduleID];
-    (void)addr;
-    (void)size;
-
-    // same as RSECCNT, no verification until the next refresh
-    RTC->RYRCNT = value;
-}
-
-static void write_RSECAR(struct mqMMIO *io, u32 addr, u32 value, int size)
-{
-    mqMachine *mach = io->userdata;
-    mqRTC *RTC = mach->modules[moduleID];
-    (void)addr;
-    (void)size;
-
-    // no check is performed on the real hardware even when the ENB bit is
-    // set. Note that you can trigger the Alarm Flag (AF) with non-valid
-    // data like 0x7f if the SECCNT have 0x7f (between two seconds)
-    RTC->RSECAR = value;
-
-    // force refresh the whole module to handle potential interruption
-    // rtc_core_refresh(RTC);
-}
-
-static void write_RMINAR(struct mqMMIO *io, u32 addr, u32 value, int size)
-{
-    mqMachine *mach = io->userdata;
-    mqRTC *RTC = mach->modules[moduleID];
-    (void)addr;
-    (void)size;
-
-    // same as RSECAR, no check performed
-    RTC->RMINAR = value;
-
-    // force refresh the whole module to handle potential interruption
-    // rtc_core_refresh(RTC);
-}
-
-static void write_RHRAR(struct mqMMIO *io, u32 addr, u32 value, int size)
-{
-    mqMachine *mach = io->userdata;
-    mqRTC *RTC = mach->modules[moduleID];
-    (void)addr;
-    (void)size;
-
-    // same as RSECAR, no check performed
-    RTC->RHRAR = value & 0xbf;
-
-    // force refresh the whole module to handle potential interruption
-    // rtc_core_refresh(RTC);
-}
-
-static void write_RWKAR(struct mqMMIO *io, u32 addr, u32 value, int size)
-{
-    mqMachine *mach = io->userdata;
-    mqRTC *RTC = mach->modules[moduleID];
-    (void)addr;
-    (void)size;
-
-    // same as RSECAR, no check performed
-    RTC->RWKAR = value & 0x87;
-
-    // force refresh the whole module to handle potential interruption
-    // rtc_core_refresh(RTC);
-}
-
-static void write_RDAYAR(struct mqMMIO *io, u32 addr, u32 value, int size)
-{
-    mqMachine *mach = io->userdata;
-    mqRTC *RTC = mach->modules[moduleID];
-    (void)addr;
-    (void)size;
-
-    // same as RSECAR, no check performed
-    RTC->RDAYAR = value & 0xbf;
-
-    // force refresh the whole module to handle potential interruption
-    // rtc_core_refresh(RTC);
-}
-
-static void write_RMONAR(struct mqMMIO *io, u32 addr, u32 value, int size)
-{
-    mqMachine *mach = io->userdata;
-    mqRTC *RTC = mach->modules[moduleID];
-    (void)addr;
-    (void)size;
-
-    // same as RSECAR, no check performed
-    RTC->RMONAR = value & 0x9f;
-
-    // force refresh the whole module to handle potential interruption
-    // rtc_core_refresh(RTC);
-}
-
-static void write_RYRAR(struct mqMMIO *io, u32 addr, u32 value, int size)
-{
-    mqMachine *mach = io->userdata;
-    mqRTC *RTC = mach->modules[moduleID];
-    (void)addr;
-    (void)size;
-
-    // same as RSECAR, no check performed
-    RTC->RYRAR = value;
-
-    // force refresh the whole module to handle potential interruption
-    // rtc_core_refresh(RTC);
-}
-
-static void write_RCR1(struct mqMMIO *io, u32 addr, u32 value, int size)
-{
-    mqMachine *mach = io->userdata;
-    mqRTC *RTC = mach->modules[moduleID];
-    (void)addr;
-    (void)size;
-
-    u8 old = RTC->RCR1;
-    RTC->RCR1 = value & 0x99;
-
-    u8 diff = RTC->RCR1 ^ old;
-    if((diff & 0x80) && (old & 0x80))
-        mq_intc_setInterruptStatus(mach, MQ_INT_RTC_CUI, false);
-    if((diff & 0x10) && (old & 0x10))
-        mq_intc_setInterruptStatus(mach, MQ_INT_RTC_CUI, false);
-    if((diff & 0x08) && (old & 0x08))
-        mq_intc_setInterruptStatus(mach, MQ_INT_RTC_ATI, false);
-    if((diff & 0x01) && (old & 0x01))
-        mq_intc_setInterruptStatus(mach, MQ_INT_RTC_ATI, false);
-}
-
-static void write_RCR2(struct mqMMIO *io, u32 addr, u32 value, int size)
-{
-    mqMachine *mach = io->userdata;
-    mqRTC *RTC = mach->modules[moduleID];
-    (void)addr;
-    (void)size;
-
-    RTC->RCR2 = value & 0xf3;
-    // todo: handle this register
-}
-
-static void write_RCR3(struct mqMMIO *io, u32 addr, u32 value, int size)
-{
-    mqMachine *mach = io->userdata;
-    mqRTC *RTC = mach->modules[moduleID];
-    (void)addr;
-    (void)size;
-
-    RTC->RCR3 = value & 0x80;
-    rtc_interrupt_refresh(mach, RTC);
-}
-
-//=====================================//
-
 // note concerning the hardware behaviour
 //   if a register, for example RMINCNT, have an invalid data like 0x0f,
 //   then the register will be corrected only when the carry occur. In our
@@ -359,90 +133,70 @@ static void write_RCR3(struct mqMMIO *io, u32 addr, u32 value, int size)
 //    RDAYCNT 0x30 -> RMINCNT Carry -> 0x11 (RMONCNT 0x00 -> invalid)
 //    RDAYCNT 0x28 -> RMINCNT Carry -> 0x01 (RYRCNT 0x1900 -> no leap year)
 //    RDAYCNT 0x28 -> RMINCNT Carry -> 0x29 (RYRCNT 0x2020 -> leap year)
-static void mq_rtc_process(mqMachine *mach, int cyclesElapsed)
+static void rtc_refresh_counters(mqMachine *mach, mqRTC *RTC)
 {
-    mqRTC *RTC = mach->modules[moduleID];
     bool carry;
-    u8 month;
     u8 year;
     u8 day;
-    u8 ten;
-    u8 sec;
+    u8 month;
+    u8 p1;
+    u8 p2;
 
-    (void)cyclesElapsed;
-
-    u64 ticks = mq_timer_update(&RTC->internalTimer);
-    if (ticks == 0)
-        return;
-    // 64Hz
-    // - add the Carry Flag (CF)
-    // - reset the register
-    carry = false;
-    RTC->R64CNT += ticks;
-    if(RTC->R64CNT >= 128) {
-        RTC->R64CNT = 0;
-        RTC->RCR1 |= 0x80;
-        carry = true;
-    }
-    if(!carry) {
-        //rtc_interrupt_refresh(mach, RTC);
-        return;
-    }
     // seconds (0 to 59)
     // - handle 60+ seconds overflow
     carry = false;
-    ten = ((RTC->RSECCNT >> 4) & 0x7);
-    sec = ((RTC->RSECCNT >> 0) & 0xf) + 1;
-    if(sec >= 10) {
-        ten += 1;
-        sec = 0;
+    p1 = ((RTC->RSECCNT >> 4) & 0x7);
+    p2 = ((RTC->RSECCNT >> 0) & 0xf);
+    if(p2 >= 10) {
+        p1 += 1;
+        p2 = 0;
     }
-    if(ten >= 6) {
-        ten = 0;
-        sec = 0;
+    if(p1 >= 6) {
+        p1 = 0;
+        p2 = 0;
         carry = true;
     }
-    RTC->RSECCNT = (ten << 4) | sec;
+    RTC->RSECCNT = (p1 << 4) | p2;
     if(!carry) {
-        rtc_interrupt_refresh(mach, RTC);
+        rtc_refresh_interrupts(mach, RTC);
         return;
     }
     // minute (00 to 59)
     // - handle 60+ minutes overflow
     carry = false;
-    ten = ((RTC->RMINCNT >> 4) & 0x7);
-    sec = ((RTC->RMINCNT >> 0) & 0xf) + 1;
-    if(sec >= 10) {
-        ten += 1;
-        sec = 0;
+    p1 = ((RTC->RMINCNT >> 4) & 0x7);
+    p2 = ((RTC->RMINCNT >> 0) & 0xf) + 1;
+    if(p2 >= 10) {
+        p1 += 1;
+        p2 = 0;
     }
-    if(ten >= 6) {
-        ten = 0;
-        sec = 0;
+    if(p1 >= 6) {
+        p1 = 0;
+        p2 = 0;
         carry = true;
     }
-    RTC->RMINCNT = (ten << 4) | sec;
+    RTC->RMINCNT = (p1 << 4) | p2;
     if(!carry) {
-        rtc_interrupt_refresh(mach, RTC);
+        rtc_refresh_interrupts(mach, RTC);
         return;
     }
     // hour (00 to 23)
     // - handle 24+ hours overflow
     carry = false;
-    ten = ((RTC->RHRCNT >> 4) & 0x3);
-    sec = ((RTC->RHRCNT >> 0) & 0xf) + 1;
-    if(sec >= 10) {
-        ten += 1;
-        sec = 0;
+    p1 = ((RTC->RHRCNT >> 4) & 0x3);
+    p2 = ((RTC->RHRCNT >> 0) & 0xf) + 1;
+    if(p2 >= 10) {
+        p1 += 1;
+        p2 = 0;
     }
-    if(ten >= 3 || (ten == 2 && sec >= 4)) {
-        ten = 0;
-        sec = 0;
+    if(p1 >= 3 || (p1 == 2 && p2 >= 4)) {
+        p1 = 0;
+        p2 = 0;
         carry = true;
     }
-    RTC->RHRCNT = (ten << 4) | sec;
+    RTC->RHRCNT = (p1 << 4) | p2;
     if(!carry) {
-        rtc_interrupt_refresh(mach, RTC);
+        rtc_refresh_interrupts(mach, RTC);
         return;
     }
     // day (01 to 31)
@@ -451,13 +205,13 @@ static void mq_rtc_process(mqMachine *mach, int cyclesElapsed)
     // - handle month that have 31 days
     // - handle month that have 30 days
     carry = false;
-    ten = ((RTC->RDAYCNT >> 4) & 0x3);
-    sec = ((RTC->RDAYCNT >> 0) & 0xf) + 1;
-    if(sec >= 10) {
-        ten += 1;
-        sec = 1;
+    p1 = ((RTC->RDAYCNT >> 4) & 0x3);
+    p2 = ((RTC->RDAYCNT >> 0) & 0xf) + 1;
+    if(p2 >= 10) {
+        p1 += 1;
+        p2 = 1;
     }
-    day   = ten * 10 + sec;
+    day   = p1 * 10 + p2;
     month = bcd8(RTC->RMONCNT);
     year  = bcd16(RTC->RYRCNT);
     if(month == 2) {
@@ -476,43 +230,290 @@ static void mq_rtc_process(mqMachine *mach, int cyclesElapsed)
         carry = (day > 31);
     }
     if(carry) {
-        ten = 0;
-        sec = 1;
+        p1 = 0;
+        p2 = 1;
     }
-    RTC->RDAYCNT = (ten << 4) | sec;
+    RTC->RDAYCNT = (p1 << 4) | p2;
     if(!carry) {
-        rtc_interrupt_refresh(mach, RTC);
+        rtc_refresh_interrupts(mach, RTC);
         return;
     }
     // day of week (0 to 6)
     // - do not reset the RDAYCNT carry since it's also used by RMONCNT
     // - no carry is performed here
-    sec = (RTC->RWKCNT & 0x03) + 1;
-    if(sec >= 7)
-        sec = 0;
-    RTC->RWKCNT = sec;
+    p2 = (RTC->RWKCNT & 0x03) + 1;
+    if(p2 >= 7)
+        p2 = 0;
+    RTC->RWKCNT = p2;
     // month (01 to 12)
     // - handle 13+ months
     carry = false;
-    ten = ((RTC->RMONCNT >> 4) & 0x1);
-    sec = ((RTC->RMONCNT >> 0) & 0xf) + 1;
-    if(sec >= 10) {
-        ten += 1;
-        sec = 1;
+    p1 = ((RTC->RMONCNT >> 4) & 0x1);
+    p2 = ((RTC->RMONCNT >> 0) & 0xf) + 1;
+    if(p2 >= 10) {
+        p1 += 1;
+        p2 = 1;
     }
-    if (ten >= 2 || (ten == 1 && sec >= 3)) {
-        ten = 0;
-        sec = 1;
+    if (p1 >= 2 || (p1 == 1 && p2 >= 3)) {
+        p1 = 0;
+        p2 = 1;
         carry = true;
     }
-    RTC->RMONCNT = (ten << 4) | sec;
+    RTC->RMONCNT = (p1 << 4) | p2;
     // year
     // - no carry is generated
     // - no check is performed
     if(carry)
         RTC->RYRCNT = bcd16(int16(RTC->RYRCNT) + 1);
     // refresh interrupt status
-    rtc_interrupt_refresh(mach, RTC);
+    rtc_refresh_interrupts(mach, RTC);
+}
+
+//==================== REGS ===========//
+
+static void write_RSECCNT(struct mqMMIO *io, u32 addr, u32 value, int size)
+{
+    mqMachine *mach = io->userdata;
+    mqRTC *RTC = mach->modules[moduleID];
+    (void)addr;
+    (void)size;
+
+    // on the real hardware, no verification is performed on the value.
+    // You can write 0x7f and the register will not perform anything until
+    // a carry occur which will correct the value as a basic overflow:
+    //   RSECCNT 0x0f -> R64CNT Carry -> 0x10
+    //   RSECCNT 0x7f -> R64CNT Carry -> 0x00 (and RMINCNT+1)
+    // this is valid for all other R*CNT registers
+    RTC->RSECCNT = value & 0x7f;
+}
+
+static void write_RMINCNT(struct mqMMIO *io, u32 addr, u32 value, int size)
+{
+    mqMachine *mach = io->userdata;
+    mqRTC *RTC = mach->modules[moduleID];
+    (void)addr;
+    (void)size;
+
+    RTC->RMINCNT = value & 0x7f;
+}
+
+static void write_RHRCNT(struct mqMMIO *io, u32 addr, u32 value, int size)
+{
+    mqMachine *mach = io->userdata;
+    mqRTC *RTC = mach->modules[moduleID];
+    (void)addr;
+    (void)size;
+
+    RTC->RHRCNT = value & 0x3f;
+}
+
+static void write_RWKCNT(struct mqMMIO *io, u32 addr, u32 value, int size)
+{
+    mqMachine *mach = io->userdata;
+    mqRTC *RTC = mach->modules[moduleID];
+    (void)addr;
+    (void)size;
+
+    RTC->RWKCNT = value & 0x07;
+}
+
+static void write_RDAYCNT(struct mqMMIO *io, u32 addr, u32 value, int size)
+{
+    mqMachine *mach = io->userdata;
+    mqRTC *RTC = mach->modules[moduleID];
+    (void)addr;
+    (void)size;
+
+    RTC->RDAYCNT = value & 0x3f;
+}
+
+static void write_RMONCNT(struct mqMMIO *io, u32 addr, u32 value, int size)
+{
+    mqMachine *mach = io->userdata;
+    mqRTC *RTC = mach->modules[moduleID];
+    (void)addr;
+    (void)size;
+
+    RTC->RMONCNT = value & 0x1f;
+}
+
+static void write_RYRCNT(struct mqMMIO *io, u32 addr, u32 value, int size)
+{
+    mqMachine *mach = io->userdata;
+    mqRTC *RTC = mach->modules[moduleID];
+    (void)addr;
+    (void)size;
+
+    RTC->RYRCNT = value;
+}
+
+static void write_RSECAR(struct mqMMIO *io, u32 addr, u32 value, int size)
+{
+    mqMachine *mach = io->userdata;
+    mqRTC *RTC = mach->modules[moduleID];
+    (void)addr;
+    (void)size;
+
+    // no check is performed on data validity on the real hardware even
+    // when the ENB bit is set. Note that you can trigger the Alarm Flag
+    // (AF) with non-valid data like 0x7f if the RSECCNT also have 0x7f
+    // (which is possible only between carry update (two second for
+    // RSECCNT))
+    RTC->RSECAR = value;
+}
+
+static void write_RMINAR(struct mqMMIO *io, u32 addr, u32 value, int size)
+{
+    mqMachine *mach = io->userdata;
+    mqRTC *RTC = mach->modules[moduleID];
+    (void)addr;
+    (void)size;
+
+    RTC->RMINAR = value;
+}
+
+static void write_RHRAR(struct mqMMIO *io, u32 addr, u32 value, int size)
+{
+    mqMachine *mach = io->userdata;
+    mqRTC *RTC = mach->modules[moduleID];
+    (void)addr;
+    (void)size;
+
+    RTC->RHRAR = value & 0xbf;
+}
+
+static void write_RWKAR(struct mqMMIO *io, u32 addr, u32 value, int size)
+{
+    mqMachine *mach = io->userdata;
+    mqRTC *RTC = mach->modules[moduleID];
+    (void)addr;
+    (void)size;
+
+    RTC->RWKAR = value & 0x87;
+}
+
+static void write_RDAYAR(struct mqMMIO *io, u32 addr, u32 value, int size)
+{
+    mqMachine *mach = io->userdata;
+    mqRTC *RTC = mach->modules[moduleID];
+    (void)addr;
+    (void)size;
+
+    RTC->RDAYAR = value & 0xbf;
+}
+
+static void write_RMONAR(struct mqMMIO *io, u32 addr, u32 value, int size)
+{
+    mqMachine *mach = io->userdata;
+    mqRTC *RTC = mach->modules[moduleID];
+    (void)addr;
+    (void)size;
+
+    RTC->RMONAR = value & 0x9f;
+}
+
+static void write_RYRAR(struct mqMMIO *io, u32 addr, u32 value, int size)
+{
+    mqMachine *mach = io->userdata;
+    mqRTC *RTC = mach->modules[moduleID];
+    (void)addr;
+    (void)size;
+
+    RTC->RYRAR = value;
+}
+
+static void write_RCR1(struct mqMMIO *io, u32 addr, u32 value, int size)
+{
+    mqMachine *mach = io->userdata;
+    mqRTC *RTC = mach->modules[moduleID];
+    (void)addr;
+    (void)size;
+
+    u8 old = RTC->RCR1;
+    RTC->RCR1 = value & 0x99;
+
+    // handle interruption clear
+    // notes
+    //   if you enable alarm interrupt which must match the configuration
+    //   (e.g RSECAR == RSECCNT), the interrupt will never occurs. This is
+    //   because, on the real hardware, the RTC check alarm each time a
+    //   R64CNT carry appear. This is why we do not performs interruption
+    //   refresh here.
+    u8 diff = RTC->RCR1 ^ old;
+    if((diff & 0x80) && (old & 0x80))
+        mq_intc_setInterruptStatus(mach, MQ_INT_RTC_CUI, false);
+    if((diff & 0x10) && (old & 0x10))
+        mq_intc_setInterruptStatus(mach, MQ_INT_RTC_CUI, false);
+    if((diff & 0x08) && (old & 0x08))
+        mq_intc_setInterruptStatus(mach, MQ_INT_RTC_ATI, false);
+    if((diff & 0x01) && (old & 0x01))
+        mq_intc_setInterruptStatus(mach, MQ_INT_RTC_ATI, false);
+}
+
+static void write_RCR2(struct mqMMIO *io, u32 addr, u32 value, int size)
+{
+    mqMachine *mach = io->userdata;
+    mqRTC *RTC = mach->modules[moduleID];
+    (void)addr;
+    (void)size;
+
+    RTC->RCR2 = (value & 0xf7) | 0x08;
+    if(RTC->RCR2 & 0x02) {
+        RTC->R64CNT = 0x00;
+        RTC->RCR2 ^= 0x02;
+    }
+    if(RTC->RCR2 & 0x04) {
+        mq_log(MQ_LOG_ERROR, "ADJ REQUEST");
+        RTC->RSECCNT = (bcd8(RTC->RSECCNT) < 30) ? 0x00 : 0x7f;
+        rtc_refresh_counters(mach, RTC);
+        RTC->RCR2 ^= 0x04;
+    }
+}
+
+static void write_RCR3(struct mqMMIO *io, u32 addr, u32 value, int size)
+{
+    mqMachine *mach = io->userdata;
+    mqRTC *RTC = mach->modules[moduleID];
+    (void)addr;
+    (void)size;
+
+    RTC->RCR3 = value & 0x80;
+    rtc_refresh_interrupts(mach, RTC);
+}
+
+//=====================================//
+
+static void mq_rtc_process(mqMachine *mach, int cyclesElapsed)
+{
+    mqRTC *RTC = mach->modules[moduleID];
+
+    (void)cyclesElapsed;
+
+    u64 ticks = mq_timer_update(&RTC->internalTimer);
+    if (ticks == 0)
+        return;
+
+    // 64Hz
+    // - add the Carry Flag (CF) only if RCR2.START=1
+    // - reset the register
+    // - handle the RCR2.START bit
+    bool carry = false;
+    RTC->R64CNT += ticks;
+    if(RTC->R64CNT >= 128) {
+        RTC->R64CNT = 0;
+        carry = true;
+    }
+    if(!carry)
+        return;
+    if((RTC->RCR2 & 0x01) == 0)
+        return;
+    RTC->RCR1 |= 0x80;
+
+    // manually add the RSECCNT carry and request a complet refresh
+    u8 p1 = ((RTC->RSECCNT >> 4) & 0x7);
+    u8 p2 = ((RTC->RSECCNT >> 0) & 0xf) + 1;
+    RTC->RSECCNT = (p1 << 4) | p2;
+    rtc_refresh_counters(mach, RTC);
 }
 
 bool mq_rtc_setup(mqMachine *mach, int initializeKind)
