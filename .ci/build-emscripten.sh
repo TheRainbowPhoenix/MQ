9dba4b9dccda6718091ed039ed1a89b7a4a04317
Yann MAGNIN <yann.magnin@protonmail.com>
2025-12-14 13:14:25 +0100

    ci: support Emscripten build
    * add basic Emscripten build script
    * add web archive in release assets
    * bump Azur dependency to fix ffmpeg unwanted build with Emscripten
    * force submodule cache in `integrity_check` step to avoid collision
      between `linux_build` and `web_build`
    * drop sha256 files generation (useless)
    * simplify the artifacts downloads

--- .ci/build-emscripten.sh
+++ .ci/build-emscripten.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Build script for Linux. This script assumes being called by the CI
+# Build script for Emscripten. This script assumes being called by the CI
 # workflow with the `ubuntu` image with all external dependencies already
 # installed.
 # Also, this script require one argument which is the current project
@@ -14,17 +14,31 @@ cd -- "$(dirname -- "${BASH_SOURCE[0]}")/.." || exit 1
 # rudimentary argument check
 [ $# -ne 1 ] && echo 'missing version information' >&2 && exit 1
 
+# manually install emsdk
+echo 'Install emsdk...'
+mkdir -p build/web/
+cd build/web/ || exit 1
+git clone https://github.com/emscripten-core/emsdk.git
+cd emsdk/ || exit 1
+./emsdk install latest
+./emsdk activate latest
+# shellcheck disable=SC1091
+# do not statically check if this file exists. It will exists during the
+# execution
+source ./emsdk_env.sh
+cd ../../../ || exit 1
+
 # manually and locally install Azur (temporary hack)
 echo 'Building Azur...'
-mkdir -p build/linux/azur
-cd build/linux/azur || exit 1
+mkdir -p build/web/azur
+cd build/web/azur || exit 1
 mkdir -p sysroot
-export AZUR_PATH_linux="$PWD/sysroot"
-cmake \
+export AZUR_PATH_emscripten="$PWD/sysroot"
+emcmake cmake \
     -B build \
     -S ../../../3rdparty/azur \
-    -DAZUR_PLATFORM=linux \
-    -DCMAKE_INSTALL_PREFIX="$AZUR_PATH_linux"
+    -DAZUR_PLATFORM=emscripten \
+    -DCMAKE_INSTALL_PREFIX="$AZUR_PATH_emscripten"
 if ! cmake --build build --target install --parallel ; then
     echo 'Unable to build Azur, abort :(' >&2
     exit 1
@@ -33,19 +47,21 @@ cd ../../../ || exit 1
 
 # manually build MQ
 echo 'Building MQ...'
-cmake \
-    -B build/linux/mq \
+emcmake cmake \
+    -B build/web/mq \
     -S . \
     -DCMAKE_BUILD_TYPE=Release \
-    -DAZUR_PLATFORM=linux \
+    -DAZUR_PLATFORM=emscripten \
     -DMQ_DISABLE_OPTIMIZATIONS=0 \
     -DMQ_PROFILING_GPROF=0 \
     -DMQ_PROFILING_TRACY=0 \
-    -DMQ_VIDEO_FFMPEG=1
-if ! cmake --build build/linux/mq --target mq --parallel ; then
+    -DMQ_VIDEO_FFMPEG=0
+if ! cmake --build build/web/mq --target mq --parallel ; then
     echo 'Unable to build MQ, abord :(' >&2
     exit 1
 fi
 
 # generate final binary information
-mv build/linux/mq/mq "./build/mq-linux-$1"
+cd build/web/mq || exit 1
+tar -cjf "mq-web-$1.tar.bz2" mq.data mq.js mq.wasm index.html
+mv "mq-web-$1.tar.bz2" ../../
