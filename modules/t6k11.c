cf6ab6f1d986733f6a09c434c7937e1cc266b0bf
Yann MAGNIN <yann.magnin@protonmail.com>
2026-01-02 16:18:31 +0100

    t6k11: dissociate T6K11 and ML9801 logic using startup detection
    Add a basic startup detection between the T6K11 and ML9801 drivers. For
    now, the detection is extremely basic and probably broken if the
    application uses undocumented registers, since I simply check if the first
    register request is a `T6K11` one or a `ML9801` one.
    
    But the idea is here, we now have a clear step for selecting the variant
    for the posterity.

--- modules/t6k11.c
+++ modules/t6k11.c
@@ -35,44 +35,85 @@ static void write_t6k11_data(mqMachine *mach, u32 value)
     if(!T6K11)
         return;
 
-    switch(T6K11->REG) {
-        case 4: { /* Y-address / X-address (depending on state) */
-            bool setX = (value >> 7) & 1;
-            if(setX)
-                T6K11->row = value & 0x3f;
-            else
-                T6K11->col = value & 0x1f;
-            break;
-        }
+    /* variant detection step.
+     * try to detect if the application request the legacy T6K11 driver or
+     * the "new" ML9801 one */
+    if(T6K11->variant == T6K11_VARIANT_NONE) {
+        if(T6K11->REG == 4 || T6K11->REG == 7)
+            T6K11->variant = T6K11_VARIANT_T6K11;
+        else
+            T6K11->variant = T6K11_VARIANT_ML9801;
+    }
 
-        case 10:
-        case 7: {  /* Data (7: T6K11 ; 10: ML9801) */
-            if(T6K11->col >= 16 || T6K11->row >= 64) {
-                mq_log(MQ_LOG_ERROR, "write_t6k11_data: out-of-bounds pixel "
-                    "write at (col %d, row %d) ", T6K11->col, T6K11->row);
-                mq_machine_setStuck(mach);
-                return;
+    if(T6K11->variant == T6K11_VARIANT_T6K11) {
+        switch(T6K11->REG) {
+            case 4: { /* Y-address / X-address (depending on state) */
+                bool setY = (value >> 7) & 1;
+                if(setY)
+                    T6K11->row = value & 0x3f;
+                else
+                    T6K11->col = value & 0x1f;
+                break;
             }
-            u8 *dst = &((u8*)mach->display->data)[(128 * T6K11->row) + (8 * T6K11->col)];
-            for(int i = 0; i < 8; i++) {
-                *dst++ = ~((i8)value >> 7);
-                value <<= 1;
+            case 7: {  /* Data */
+                if(T6K11->col >= 16 || T6K11->row >= 64) {
+                    mq_log(MQ_LOG_ERROR, "write_t6k11_data: out-of-bounds pixel "
+                        "write at (col %d, row %d) ", T6K11->col, T6K11->row);
+                    mq_machine_setStuck(mach);
+                    return;
+                }
+                u8 *dst = &((u8*)mach->display->data)[(128 * T6K11->row) + (8 * T6K11->col)];
+                for(int i = 0; i < 8; i++) {
+                    *dst++ = ~((i8)value >> 7);
+                    value <<= 1;
+                }
+                T6K11->col++;
+                mq_display_setDirty(mach->display, true);
+                break;
             }
-            T6K11->col++;
-            mq_display_setDirty(mach->display, true);
-            break;
+            default: {
+                mq_log(MQ_LOG_DEBUG, "write_t6k11_data: REG: %02x, value: %08x "
+                    "(register not implemented)", T6K11->REG, value);
+            }
         }
-        case 8: { /* Y-address / X-address (ML9801 driver only) */
-            bool setY = (value >> 7) & 1;
-            if(setY)
-                T6K11->row = value & 0x3f;
-            else
-                T6K11->col = (value & 0x1f) - 4;
-            break;
+    }
+    else if(T6K11->variant == T6K11_VARIANT_ML9801) {
+        switch(T6K11->REG) {
+            case 8: { /* Y-address / X-address */
+                bool setY = (value >> 7) & 1;
+                if(setY)
+                    T6K11->row = value & 0x3f;
+                else
+                    T6K11->col = (value & 0x1f) - 4;
+                break;
+            }
+            case 10: {  /* Data */
+                if(T6K11->col >= 16 || T6K11->row >= 64) {
+                    mq_log(MQ_LOG_ERROR, "write_t6k11_data: out-of-bounds pixel "
+                        "write at (col %d, row %d) ", T6K11->col, T6K11->row);
+                    mq_machine_setStuck(mach);
+                    return;
+                }
+                u8 *dst = &((u8*)mach->display->data)[(128 * T6K11->row) + (8 * T6K11->col)];
+                for(int i = 0; i < 8; i++) {
+                    *dst++ = ~((i8)value >> 7);
+                    value <<= 1;
+                }
+                T6K11->col++;
+                mq_display_setDirty(mach->display, true);
+                break;
+            }
+            default: {
+                mq_log(MQ_LOG_DEBUG, "write_ml9801_data: REG: %02x, value: %08x "
+                    "(register not implemented)", T6K11->REG, value);
+            }
         }
-        default:
-            mq_log(MQ_LOG_DEBUG, "write_t6k11_data: REG: %02x, value: %08x "
-                "(register not implemented)", T6K11->REG, value);
+    }
+    else {
+        mq_log(MQ_LOG_ERROR,
+            "write_t6k11_data: unknown driver variant %d", T6K11->variant);
+        mq_machine_setStuck(mach);
+        return;
     }
 }
 
@@ -86,6 +127,7 @@ bool mq_t6k11_setup(mqMachine *mach)
     mqT6K11 *T6K11 = calloc(1, sizeof *T6K11);
     if(!T6K11)
         return false;
+    T6K11->variant = T6K11_VARIANT_NONE;
 
     bool ok = true;
     ok &= mq_page_mapRegister8(pgb4000, "T6K11_SEL", 0xb4000000, NULL,
